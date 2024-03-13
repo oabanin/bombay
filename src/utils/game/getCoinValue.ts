ea806fdd4e719c18f0c1bfd3920908bf62890261
Oleh Abanin <oleh@outvio.com>
2024-03-13 23:33:02 +0200

    fix: coin value

--- src/utils/game/getCoinValue.ts
+++ src/utils/game/getCoinValue.ts
@@ -3,8 +3,6 @@ import numeral from 'numeral';
 export const getCoinValue = (value: number) => {
   if (value >= 99000) {
     return '99k+';
-  } else if (value >= 10000) {
-    return numeral(value).format('0a', Math.floor);
   } else if (value >= 1000) {
     return numeral(value).format('0.0a', Math.floor);
   } else {
